a808a6be09c18123855eea6de8880cd74463df98
Sjoerd Simons <sjoerd@collabora.com>
2023-01-14 21:06:33 +0100

    Add tests for the sync download functionality
    Create a mock io device that behaves like a DFU device as well as doing
    various sanity checks in how it's driven. This is a precursor to start
    changing the state machine to improve compatibility.
    
    This only tests for devices with manifestation_tolerant false as the
    current code doesn't handle manifestation_tolerant true correctly.
    
    Signed-off-by: Sjoerd Simons <sjoerd@collabora.com>

---
+++ tests/download.rs
@@ -0,0 +1,42 @@
+use mock::MockIO;
+
+mod mock;
+
+fn setup() {
+    let _ = env_logger::builder()
+        .is_test(true)
+        .filter_level(log::LevelFilter::Trace)
+        .parse_default_env()
+        .try_init();
+}
+
+fn test_simple_download(mock: MockIO) {
+    let firmware = b"thisisnotafirmwareorisit";
+    let mut dfu = dfu_core::sync::DfuSync::new(mock, 0x0);
+
+    dfu.download_from_slice(firmware).unwrap();
+    let (mock, _) = dfu.into_parts();
+
+    assert!(mock.completed());
+    assert_eq!(firmware, mock.downloaded().as_slice());
+}
+
+#[test]
+fn no_will_detach_and_no_manifestation_toleration() {
+    setup();
+    let mock = mock::MockIOBuilder::default()
+        .will_detach(false)
+        .manifestation_tolerant(false)
+        .build();
+    test_simple_download(mock);
+}
+
+#[test]
+fn will_detach_and_no_manifestation_toleration() {
+    setup();
+    let mock = mock::MockIOBuilder::default()
+        .manifestation_tolerant(false)
+        .will_detach(true)
+        .build();
+    test_simple_download(mock);
+}
